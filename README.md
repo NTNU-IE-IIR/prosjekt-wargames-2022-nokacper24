f8ec9a5e6e976d5d2c5ce4a1c186d01aed526cfc
Kacper Lukasz Nowicki <kacperln@stud.ntnu.no>
2022-02-19 22:33:11 +0100

    created Unit class, not all methods completed yet

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Wargames
-This is a programming project in IDATA2001 at NTNU.
+This is a programming project in IDATA2001 at NTNU.  
 This readme file is to be updated.
--- src/main/java/no/ntnu/idata2001/Unit.java
+++ src/main/java/no/ntnu/idata2001/Unit.java
@@ -1,4 +1,88 @@
 package no.ntnu.idata2001;
 
-public class Unit {
+/**
+ * This abstract class represents a single unit in the Wargames app.
+ */
+public abstract class Unit {
+
+  private final String name;
+  private int health;
+  private final int attack;
+  private final int armor;
+
+  /**
+   * Constructor of the abstract class Unit.
+   *
+   * @param name   Name of the unit
+   * @param health Initial health of the unit
+   * @param attack Base attack damage
+   * @param armor  Base defence value
+   */
+  protected Unit(String name, int health, int attack, int armor) {
+    this.name = name;
+    this.health = health;
+    this.attack = attack;
+    this.armor = armor;
+  }
+
+  /**
+   * Returns name of the unit.
+   *
+   * @return name
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * Returns current health of the unit.
+   *
+   * @return health
+   */
+  public int getHealth() {
+    return this.health;
+  }
+
+  /**
+   * Returns attack damage.
+   *
+   * @return attack
+   */
+  public int getAttack() {
+    return attack;
+  }
+
+  /**
+   * Returns armor value of the unit.
+   *
+   * @return armor
+   */
+  public int getArmor() {
+    return this.armor;
+  }
+
+  /**
+   * Calculates and sets health of the unit attacked by this unit.
+   *
+   * @param opponent unit to be attacked
+   */
+  public void attack(Unit opponent) {
+    opponent.health = opponent.health - (this.attack + this.getAttackBonus())
+        + (opponent.armor + opponent.getResistBonus());
+  }
+
+  /**
+   * Return attack bonus of the unit.
+   *
+   * @return attack bonus
+   */
+  abstract int getAttackBonus();
+
+  /**
+   * Returns resist bonus of the unit.
+   *
+   * @return resist bonus
+   */
+  abstract int getResistBonus();
+
 }
