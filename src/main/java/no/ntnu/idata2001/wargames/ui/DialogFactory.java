fa4abc3156dae8aa2a8e58b064d85a42605e0e92
Kacper Lukasz Nowicki <kacperln@stud.ntnu.no>
2022-05-16 16:10:36 +0200

    feat(DialogFactory.java,WarGames.java): added error dialog when exception occurs when loading resources
    aditionally added icon

--- src/main/java/no/ntnu/idata2001/wargames/ui/DialogFactory.java
+++ src/main/java/no/ntnu/idata2001/wargames/ui/DialogFactory.java
@@ -1,7 +1,9 @@
 package no.ntnu.idata2001.wargames.ui;
 
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  * Class responsible for creating various dialogs used in WarGamesApplication.
@@ -11,13 +13,14 @@ public class DialogFactory {
   /**
    * Create an error dialog with given header and content.
    *
-   * @param header header of the dialog
+   * @param header  header of the dialog
    * @param content content of the dialog
    * @return error dialog
    */
   public Alert createErrorDialog(String header, String content) {
     Alert alert = new Alert(Alert.AlertType.ERROR);
     alert.setTitle("Error");
+    this.setAlertWindowIcon(alert);
     alert.setHeaderText(header);
     alert.setContentText(content);
     return alert;
@@ -26,13 +29,14 @@ public class DialogFactory {
   /**
    * Create a confirmation dialog with given header and content.
    *
-   * @param header header of the dialog
+   * @param header  header of the dialog
    * @param content content of the dialog
    * @return confirmation dialog
    */
   public Alert createConfirmationDialog(String header, String content) {
     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
     alert.setTitle("Confirmation");
+    this.setAlertWindowIcon(alert);
     alert.setHeaderText(header);
     alert.setContentText(content);
     return alert;
@@ -50,6 +54,7 @@ public class DialogFactory {
   public Alert createConfirmExitDialog() {
     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
     alert.setTitle("Confirm exit");
+    this.setAlertWindowIcon(alert);
     alert.setHeaderText("Are you sure you want to exit?");
     alert.setContentText("The application will be closed.");
     return alert;
@@ -58,13 +63,14 @@ public class DialogFactory {
   /**
    * Create a file chooser dialog.
    *
-   * @param header header of the dialog
+   * @param header  header of the dialog
    * @param content content of the dialog
    * @return information dialog
    */
   public Alert createInformationDialog(String header, String content) {
     Alert alert = new Alert(Alert.AlertType.INFORMATION);
     alert.setTitle("Information");
+    this.setAlertWindowIcon(alert);
     alert.setHeaderText(header);
     alert.setContentText(content);
     return alert;
@@ -81,7 +87,7 @@ public class DialogFactory {
     FileChooser fileChooser = new FileChooser();
     fileChooser.setTitle("Open Army File");
     fileChooser.getExtensionFilters().add(
-        new FileChooser.ExtensionFilter("CSV","*.csv"));
+        new FileChooser.ExtensionFilter("CSV", "*.csv"));
     return fileChooser;
   }
 
@@ -101,6 +107,26 @@ public class DialogFactory {
         new FileChooser.ExtensionFilter("CSV", "*.csv"));
     fileChooser.setInitialFileName(armyName + ".csv");
     return fileChooser;
+  }
+
+  private void setAlertWindowIcon(Alert alert) {
+    Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+    try {
+      Image icon = new Image("no/ntnu/idata2001/wargames/ui/icon.png");
+      stage.getIcons().add(icon);
+    } catch (Exception e) {
+      showResourceErrorDialog(e);
+    }
+  }
+
+  public void showResourceErrorDialog(Exception e) {
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+    alert.setTitle("Error");
+    alert.setHeaderText("Could not load resource");
+    alert.setContentText(
+        "Could not find files necessary for the application.\nPlease contact support.\nDetails: " +
+            e.getMessage());
+    alert.showAndWait();
   }
 
 }
--- src/main/java/no/ntnu/idata2001/wargames/ui/WarGames.java
+++ src/main/java/no/ntnu/idata2001/wargames/ui/WarGames.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class WarGames extends Application {
@@ -18,12 +19,15 @@ public class WarGames extends Application {
     Parent root = null;
     try {
       root = FXMLLoader.load(getClass().getResource("wargamesGui.fxml"));
+      Image icon = new Image("no/ntnu/idata2001/wargames/ui/icon.png");
+      primaryStage.getIcons().add(icon);
 
       primaryStage.setTitle("War Games");
       primaryStage.setScene(new Scene(root));
       primaryStage.show();
-    } catch (IOException e) {
-      System.out.println("ERROR: " + e.getMessage());
+    } catch (Exception e) {
+      DialogFactory dialogFactory = new DialogFactory();
+      dialogFactory.showResourceErrorDialog(e);
     }
 
     setUpDialogOnClose(primaryStage);
