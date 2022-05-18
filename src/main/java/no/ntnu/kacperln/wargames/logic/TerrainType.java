261a2fabb8dfd0ad9013025d1c17f159417f9f4f
Kacper Lukasz Nowicki <kacperln@stud.ntnu.no>
2022-05-18 13:32:08 +0200

    feat(TerrainType.java): added terraintype enum

---
+++ src/main/java/no/ntnu/kacperln/wargames/logic/TerrainType.java
@@ -0,0 +1,19 @@
+package no.ntnu.kacperln.wargames.logic;
+
+/**
+ * Enum for terrain types.
+ */
+public enum TerrainType {
+  /**
+   * Hill terrain type.
+   */
+  HILL,
+  /**
+   * Plains terrain type.
+   */
+  PLAINS,
+  /**
+   * Forest terrain type.
+   */
+  FOREST
+}
