1fbe6c8c6079084024772a4f5f692d6382d2be71
Kacper Lukasz Nowicki <kacperln@stud.ntnu.no>
2022-05-19 16:37:48 +0200

    test(BattleTest.java): implemented tests for getwinner

--- src/test/java/no/ntnu/kacperln/wargames/logic/BattleTest.java
+++ src/test/java/no/ntnu/kacperln/wargames/logic/BattleTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
@@ -101,5 +102,47 @@ class BattleTest {
     Battle battle = new Battle(army1, army2, TerrainType.values()[0]);
 
     assertNull(battle.simulate());
+  }
+
+  @Test
+  void testGetWInner() {
+    ArrayList<Unit> collectionOfUnits1 = new ArrayList<>();
+    Unit unit1 = new CavalryUnit("name1", 100);
+    Unit unit2 = new RangedUnit("name2", 120);
+    collectionOfUnits1.add(unit1);
+    collectionOfUnits1.add(unit2);
+
+    ArrayList<Unit> collectionOfUnits2 = new ArrayList<>();
+    Unit unit3 = new CommanderUnit("name3", 10);
+    Unit unit4 = new InfantryUnit("name4", 10);
+    collectionOfUnits2.add(unit3);
+    collectionOfUnits2.add(unit4);
+
+    Army army1 = new Army("Army1", collectionOfUnits1);
+    Army army2 = new Army("Army2", collectionOfUnits2);
+
+    Battle battle = new Battle(army1, army2, TerrainType.values()[0]);
+    battle.simulate();
+
+    assertEquals(army1, battle.getWinner());
+  }
+
+  @Test
+  void testGetWinnerBattleInProgress() {
+    ArrayList<Unit> collectionOfUnits1 = new ArrayList<>();
+    Unit unit1 = new CavalryUnit("name1", 100);
+    collectionOfUnits1.add(unit1);
+
+    ArrayList<Unit> collectionOfUnits2 = new ArrayList<>();
+    Unit unit2 = new CommanderUnit("name2", 100);
+    collectionOfUnits2.add(unit2);
+
+    Army army1 = new Army("Army1", collectionOfUnits1);
+    Army army2 = new Army("Army2", collectionOfUnits2);
+
+    Battle battle = new Battle(army1, army2, TerrainType.values()[0]);
+    battle.simulateStep();
+
+    assertThrows(BattleStillInProgressException.class, () -> battle.getWinner());
   }
 }
